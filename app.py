fbc111f58ffaa42d3d21517cab31c2b6f0760732
maniksaluja <69381906+maniksaluja@users.noreply.github.com>
2024-11-26 21:35:46 +0530

    Update app.py

--- app.py
+++ app.py
@@ -16,4 +16,4 @@ def payment_status():
     return json.dumps({"status": "success"}), 200
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5001)
