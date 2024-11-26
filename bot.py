7422f6782f488cd5568cde8bf74d7d245c3a92f2
maniksaluja <69381906+maniksaluja@users.noreply.github.com>
2024-11-26 19:18:56 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,7 +1,7 @@
 from flask import Flask, jsonify
 import requests
 
-app = Flask(__name__)
+app = Flask(manik)
 
 app_id = '73553954db925af2b456a26e07935537'
 secret_key = 'cfsk_ma_prod_2d76755985f4b26b8a93f770157c6514_167eab6c'
