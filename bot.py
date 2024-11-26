ad10970af3d72043e495034e28c66fc9476d8b75
maniksaluja <69381906+maniksaluja@users.noreply.github.com>
2024-11-26 21:53:58 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -93,5 +93,6 @@ if __name__ == "__main__":
     flask_thread = threading.Thread(target=run_flask)
     flask_thread.start()
 
-    # Run Telegram bot with asyncio
-    asyncio.run(main())
+    # Run the Telegram bot task without blocking
+    asyncio.create_task(main())
+    flask_thread.join()
